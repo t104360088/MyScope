d16d8690e968b8c22dc1f339b12338cb1bdc5078
t104360088 <z8631989816@gmail.com>
2019-06-18 00:47:26 +0800

    Fix Friend structure avatar field maybe null issue

--- app/src/main/java/com/example/myscope/manager/friend/Friend.kt
+++ app/src/main/java/com/example/myscope/manager/friend/Friend.kt
@@ -3,7 +3,7 @@ package com.example.myscope.manager.friend
 data class Friend(val uid: String = "",
                   val targetUID: String = "",
                   val name: String = "",
-                  val avatar: String = "",
+                  val avatar: String? = "",
                   val status: Int = -1)
 
 data class FriendList(val list: Array<Friend?> = arrayOf())
